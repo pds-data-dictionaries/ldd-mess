dca7bf356e13584a896861d30124e0f327c2837d
mnhughes <89472121+mnhughes@users.noreply.github.com>
2026-02-13 13:19:31 -0600

    Edits to doc directory

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Example Namespace'
+project = 'Messenger Mission LDD'
 # copyright = overidden by CSS specified below
 author = 'NASA Planetary Data System'
 
